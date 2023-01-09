1f9544b478cbfc496c71c2ada9af61cbd3f6825d
Mixail-laboratory <spectrafa@gmail.com>
2023-01-09 05:02:29 +0300

    some changes

--- calculator.py
+++ calculator.py
@@ -4,14 +4,20 @@ from fompy.models import *
 from fompy.constants import *
 
 class Silicon_n_type:
-    def __init__(self) -> None:
-        self.si = DopedSemiconductor(Si,0, 0, 1e15, from_unit(1.12, "eV"))
+    def __init__(self, conc, energy) -> None:
+        self.si = DopedSemiconductor(Si, 0, 0, conc, Si.Eg - from_unit(energy, "eV"))
         self.T = 300
-    def set_donor_energy(self, donor_energy=1.12):
-        self.si.Ed = from_unit(donor_energy, "eV")
 
     def get_fermi_level(self, temperature=300):
         return self.si.fermi_level(temperature)
+        
+    def get_params(self, temperature=300):
+        fermi_eneregy = self.si.fermi_level(temperature)
+        electrons_conc = self.si.n_concentration(fermi_eneregy, temperature)
+        return to_unit(fermi_eneregy, "eV"), electrons_conc
 
-    def set_donor_concenctration(self, donor_concentration=1e15):
-        self.si.Nd = donor_concentration
+    def get_Eg(self):
+        return to_unit(Si.Eg, "eV")
+
+    def get_energy_diff(self):
+        return to_unit(self.si.Ed, "eV")
--- main.py
+++ main.py
@@ -1,10 +1,94 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from window import MainWindow
-import warnings
-if __name__ == '__main__':
-    warnings.filterwarnings("ignore")
-    app = QApplication(sys.argv)
-    wind = MainWindow()
-    wind.show()
-    sys.exit(app.exec_())
+from tkinter import *
+import matplotlib as plt
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg,
+    NavigationToolbar2Tk
+)
+
+from fompy import units
+
+import numpy as np
+from calculator import *
+from calculator import Silicon_n_type
+temperatures = np.arange(10, 1200, 50)
+
+fermi_levels = []
+
+electrons_concentration = []
+
+
+
+def show_values(dummy=0):
+    fermi_levels.clear()
+    electrons_concentration.clear()
+    semiconductor = Silicon_n_type(w2.get(), w1.get())
+    for temp in temperatures:
+
+        fermi, concentation = semiconductor.get_params(temp)
+        fermi_levels.append(fermi)
+        electrons_concentration.append(concentation)
+    print(to_unit(semiconductor.get_fermi_level(), "eV"))
+    axes.clear()
+    axes_2.clear()
+    axes.plot(temperatures, fermi_levels)
+    axes.axhline(y = semiconductor.get_Eg(), color = 'r', linestyle = '-')
+    axes.axhline(y = semiconductor.get_energy_diff(), color = 'r', linestyle = 'dotted')
+     
+    axes.set_ylabel('Fermi level, eV')
+    axes.set_xlabel('Temperature, K')
+
+        
+    axes_2.plot(temperatures, electrons_concentration)
+    axes_2.set_xlabel('Temperature, K')
+    axes_2.set_ylabel('Electrons concetration, 1/cm^-3')
+
+    figure_canvas.draw()
+    figure_canvas_2.draw()
+
+    
+
+
+
+master = Tk()
+master.title("Task4")
+
+master.geometry("1200x700")
+
+        # create a figure
+figure = Figure(figsize=(5, 2), dpi=100)
+
+figure_2 = Figure(figsize=(5, 2), dpi=100)
+
+        # create FigureCanvasTkAgg object
+figure_canvas = FigureCanvasTkAgg(figure, master)
+
+figure_canvas_2 = FigureCanvasTkAgg(figure_2, master)
+
+
+
+axes = figure.add_subplot()
+axes_2 = figure_2.add_subplot()
+        # create the barchart
+#axes.plot(temperatures, fermi_levels)
+axes.set_ylabel('Fermi level, eV')
+axes.set_xlabel('Temperature, K')
+axes_2.set_xlabel('Temperature, K')
+axes_2.set_ylabel('Electrons concetration, 1/cm^-3')
+
+figure_canvas.get_tk_widget().pack(side=LEFT, fill=BOTH, expand=1)
+
+figure_canvas_2.get_tk_widget().pack(side=RIGHT, fill=BOTH, expand=1)
+
+
+w1 = Scale(master, from_=0.1, to=1.12, orient=HORIZONTAL, resolution=0.01, label="Donor energy, eV", command=show_values)
+w1.set(0.5)
+w1.pack(side=TOP)
+w2 = Scale(master, from_=1e+15, to=1e+20, orient=HORIZONTAL, resolution=1e+7, label="Donor concentration", command=show_values)
+w2.set(1e+17)
+w2.pack(side=TOP)
+
+
+
+master.mainloop()
